58e3a5cca14b5c128d04b4a12cf15c45fe387e6c
Balint Dolla-Takacs <balint@apexlab.io>
2024-03-13 00:02:05 +0100

    feat: setup clerk on frontend

--- apps/admin/src/app/index.tsx
+++ apps/admin/src/app/index.tsx
@@ -1,7 +1,23 @@
-import * as React from "react";
+import { SignIn, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
+import { Box } from '@mui/material';
+import AppBar from '@mui/material/AppBar';
+import React from 'react';
 
 function App(): JSX.Element {
-  return <h1>Hello world!</h1>;
+  return (
+    <>
+      <SignedOut>
+        <Box sx={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <SignIn />
+        </Box>
+      </SignedOut>
+      <SignedIn>
+        <AppBar sx={{ p: 2, alignItems: 'flex-end' }}>
+          <UserButton />
+        </AppBar>
+      </SignedIn>
+    </>
+  );
 }
 
 export default App;
--- apps/admin/src/main.tsx
+++ apps/admin/src/main.tsx
@@ -1,16 +1,26 @@
-import * as React from "react";
-import { createRoot } from "react-dom/client";
-import App from "./app";
-import "./index.css";
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import { ClerkProvider } from '@clerk/clerk-react';
+import App from './app';
+import './index.css';
 
-const el = document.getElementById("root");
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Missing Publishable Key');
+}
+
+const el = document.getElementById('root');
+
 if (el) {
   const root = createRoot(el);
   root.render(
     <React.StrictMode>
-      <App />
+      <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+        <App />
+      </ClerkProvider>
     </React.StrictMode>,
   );
 } else {
-  throw new Error("Could not find root element");
+  throw new Error('Could not find root element');
 }
