f282090009e22822d45d17741432870bd3e7389f
Balint Dolla-Takacs <balint@apexlab.io>
2024-03-14 11:30:38 +0100

    fix: layout

---
+++ apps/admin/src/app/components/layout.tsx
@@ -0,0 +1,28 @@
+import AppBar from '@mui/material/AppBar';
+import IconButton from '@mui/material/IconButton';
+import React from 'react';
+import { Link, Outlet } from 'react-router-dom';
+import HomeIcon from '@mui/icons-material/Home';
+import { UserButton } from '@clerk/clerk-react';
+import Box from '@mui/material/Box';
+import { QuestionProvider } from '../contexts/QuestionContext';
+
+const Layout = () => (
+  <>
+    <AppBar sx={{ p: 2, flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
+      <IconButton component={Link} to="/" sx={{ color: 'white' }}>
+        <HomeIcon fontSize="large" />
+      </IconButton>
+
+      <UserButton />
+    </AppBar>
+
+    <Box sx={{ mt: 10, p: 4 }}>
+      <QuestionProvider>
+        <Outlet />
+      </QuestionProvider>
+    </Box>
+  </>
+);
+
+export default Layout;
--- apps/admin/src/app/index.tsx
+++ apps/admin/src/app/index.tsx
@@ -1,25 +1,29 @@
-import { SignIn, SignedIn, SignedOut, UserButton } from '@clerk/clerk-react';
-import { createBrowserRouter, Link, RouterProvider } from 'react-router-dom';
-import { Box } from '@mui/material';
-import AppBar from '@mui/material/AppBar';
+import { SignIn, SignedIn, SignedOut } from '@clerk/clerk-react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import React from 'react';
 import Dashboard from './pages/dashboard';
 import CreateQuestion from './pages/createQuestion';
 import EditQuestion from './pages/editQuestion';
-import { QuestionProvider } from './contexts/QuestionContext';
+import Box from '@mui/material/Box';
+import Layout from './components/layout';
 
 const router = createBrowserRouter([
   {
-    path: '/',
-    element: <Dashboard />,
-  },
-  {
-    path: 'create',
-    element: <CreateQuestion />,
-  },
-  {
-    path: 'edit/:id',
-    element: <EditQuestion />,
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Dashboard />,
+      },
+      {
+        path: 'create',
+        element: <CreateQuestion />,
+      },
+      {
+        path: 'edit/:id',
+        element: <EditQuestion />,
+      },
+    ],
   },
 ]);
 
@@ -31,17 +35,7 @@ const App = () => (
       </Box>
     </SignedOut>
     <SignedIn>
-      <AppBar sx={{ p: 2, flexDirection: 'row', justifyContent: 'space-between' }}>
-        {/* <Link to="/">Dashboard</Link> */}
-
-        <UserButton />
-      </AppBar>
-
-      <Box sx={{ mt: 8, p: 4 }}>
-        <QuestionProvider>
-          <RouterProvider router={router} />
-        </QuestionProvider>
-      </Box>
+      <RouterProvider router={router} />
     </SignedIn>
   </>
 );
